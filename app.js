896cadc2063cd3ef707ee2d11df7325297e823a2
Muhammad Usman <112246337+Usman1361@users.noreply.github.com>
2023-09-25 10:55:20 +0500

    finalupload

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from "express";
 import cors from "cors";
 import { adminRouter, authRouter, servceRouter } from "./routes/index.js";
+import fileUpload from "express-fileupload";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 app.use("/upload", cors(), express.static("./upload"));
 app.use("/", authRouter);
 app.use("/", adminRouter);
